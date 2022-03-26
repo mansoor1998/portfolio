7d4c4dd00355a2891c2cb345644cdf8efaa52313
Mansoor Ali Soomro <mk.soomro@yahoo.com>
2022-03-26 18:21:51 +0100

    changes

--- custom-webpack.config.js
+++ custom-webpack.config.js
@@ -5,18 +5,9 @@ const BrotliPlugin = require(`brotli-webpack-plugin`);
 const path = require(`path`);
 module.exports = {
     plugins: [
-        new BrotliPlugin({
-            asset: '[fileWithoutExt].[ext].br',
-            test: /\.(js|css|html|svg|txt|eot|otf|ttf|gif)$/
-        }),
-        new CompressionPlugin({
-            test: /\.(js|css|html|svg|txt|eot|otf|ttf|gif)$/,
-            filename(info) {
-                let opFile = info.path.split('.'),
-                    opFileType = opFile.pop(),
-                    opFileName = opFile.join('.');
-                return `${opFileName}.${opFileType}.gzip`;
-            }
-        })
+        // new BrotliPlugin(),
+        // new CompressionPlugin({
+        //     algorithm: 'gzip'
+        // })
     ]
 }
